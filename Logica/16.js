4b73a6d12359fb2b06706848eb609ec230b6f601
pedrolucastp <84178507+pedrolucastp@users.noreply.github.com>
2021-09-22 13:24:48 -0300

    exercicios 10 e 18 atualizados

--- Logica/16.js
+++ Logica/16.js
@@ -1,3 +1,3 @@
-// Gere chaves e pontuações aleatórias (de 0 a 3 vitórias)
-// para 4 chaves esportivas de 4 países cada e em seguida filtre 
-// os 2 melhores times de cada chave que avançarão para a próxima fase.
+/* Gere chaves e pontuações aleatórias (de 0 a 3 vitórias)
+para 4 chaves esportivas de 4 países cada e em seguida filtre 
+os 2 melhores times de cada chave que avançarão para a próxima fase. */
